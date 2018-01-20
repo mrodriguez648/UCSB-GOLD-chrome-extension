895296b8bd156dfd8bc76d57819dab45713a0073
CrisCab <cristobal_caballero@umail.ucsb.edu>
2018-01-20 15:06:54 -0800

    Add files via upload
    -Extracts class data (ex. "CMPSC 165A - Artificial Intelligence")
    -Creates two arrays to store class department and class numbers; should coincide on i^th element
              -Need to parse to perform ^^
    -changes web page to textbook finding page

---
+++ extractData.js
@@ -0,0 +1,17 @@
+// JavaScript source code
+// SB Hacks 2018 1/19/18 - 1/21/18 
+
+var classTitles = document.$(".courseTitle > span")
+
+for(var i = 0; i < classTitles.length; i++){
+ 	finalTitles[i] = classTitles[i].textContent   
+}
+
+var deps = new Array() // array to hold department letters
+var classNums = new Array() // array to hold class numbers
+//match deps with classNums so ith element coincides
+
+//parse here
+
+window.location = 'http://www.ucsbstuff.com/SelectTermDept.aspx#?options=go_here';
+
