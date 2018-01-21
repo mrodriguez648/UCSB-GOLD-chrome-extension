0aea479fe64fbc9a30d14ef3fbddfadf115d7b3b
Karl Wang <kovl.void@gmail.com>
2018-01-20 21:43:07 -0800

    store stuff

--- extractData.js
+++ extractData.js
@@ -1,11 +1,11 @@
 // JavaScript source code
 // SB Hacks 2018 1/19/18 - 1/21/18 
+(function(){
+		
+	var classTitles = $(".courseTitle > span")
 
-var classTitles = $(".courseTitle > span")
+	var schdle = [];
 
-var schdle = [];
-
-let splitSchedule = function() {
 	for(var i = 0; i < classTitles.length; i++) {
 		var str = classTitles[i];
 		var splitStr = str.textContent.split(" ").filter(word => word.length > 0 && word != "-");
@@ -13,7 +13,7 @@ let splitSchedule = function() {
 		schdle[i][0] = splitStr[0];
 		schdle[i][1] = splitStr[1];
 	}
-}
 
-splitSchedule();
-
+	chrome.storage.local.set(schdle);
+	
+})();
