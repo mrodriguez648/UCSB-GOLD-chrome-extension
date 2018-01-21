f244f6679d9a9bcf30e85734d052b3ed6ae078e0
CrisCab <cristobal_caballero@umail.ucsb.edu>
2018-01-20 22:56:40 -0800

    update

--- select-textbook.js
+++ select-textbook.js
@@ -1,20 +1,46 @@
 (function(){
+    
+    //chrome.storage.local.get(schdle);
+    var schdle = [["CMPSC", "130A"],["CMPSC","130B"],["CHIN","32"]]
 
-let clickOption = function(searchString, selectClass){
-	let selectElement = $("select." + selectClass)[0];
-	selectElement.value = $("option:contains(" + searchString + ")", selectElement)[0].value;
-	if(selectElement.onchange){
-		selectElement.onchange();
-	}
-}
-clickOption("WINTER 18", "select-term");
-setTimeout(function () {
-	clickOption("CMPSC", "select-dept");  
-	setTimeout(function () {
-		clickOption("130A", "select-section");
-		$("#ctl00_ctl00_Content_Content_courseSelect_btnAddCourseToList").click();
-		$("#ctl00_ctl00_Content_Content_btnGetCourseMaterials").click();
-	}, 1000);
-}, 1000);
+    let clickOption = function(searchString, selectClass){
+	    let selectElement = $("select." + selectClass)[0];
+	    selectElement.value = $("option:contains(" + searchString + ")", selectElement)[0].value;
+	    if(selectElement.onchange){
+		    selectElement.onchange();
+	    }
+    }
+
+    let generatePromise = function (department, clas) {
+        return new Promise( function (resolve){
+            clickOption("WINTER 18", "select-term");
+            console.log("select-term")
+            setTimeout(function () {
+                clickOption(department, "select-dept");
+                console.log("select-dept" + department)
+                setTimeout(function () {
+                    clickOption(clas, "select-section");
+                    console.log("select-sect" + clas)
+                    $("#ctl00_ctl00_Content_Content_courseSelect_btnAddCourseToList").click();
+                    setTimeout(function(){
+                        resolve(department + clas);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        })   
+    }
+
+
+    async function addBooks(schdle){
+        for(var i = 0; i < schdle.length; i++){
+            console.log(i)
+            let something = await generatePromise(schdle[i][0], schdle[i][1]);
+            console.log(something)
+        }
+        $("#ctl00_ctl00_Content_Content_btnGetCourseMaterials").click();
+    };
+
+    addBooks(schdle);
+
 
 })();
